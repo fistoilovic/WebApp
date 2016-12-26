ea01003588ab2315bab5042bd378ead97070d59e
fistoilovic <fstoilovic@gmail.com>
2016-12-26 13:19:05 +0100

    Update app.clj

--- webapplication/src/webapplication/app.clj
+++ webapplication/src/webapplication/app.clj
@@ -7,6 +7,64 @@
 
 (defonce korisnici (atom {}))
 
+(defonce ukupanBMI 0)
+
+(defonce ukupnaVisina 0)
+
+(defonce ukupnaTezina 0)
+
+(defonce brojMerenihKorisnika 0)
+
+(def razlikaMV)
+
+(def tekst "")
+
+(def text "")
+
+(def prazanString "---------------")
+
+(def kime "" "korisnice")
+
+(def tekstVisina "Unesite Vrednosti Za Visinu" )
+
+(def tekstTezina "Unesite Vrednosti Za Tezinu")
+
+(def tekstBMI "Kliknite na Dugme Izracunaj!!! ")
+
+(defn razlika 
+  [a b]
+  (if(= a b)
+     (do (def razlikaMV "ista")
+       (- a b))
+     (if (< a b)
+       (do (def razlikaMV "veca")
+         (- b a))
+       (do (def razlikaMV "manja")
+         (- a b)))))
+
+(defn provera 
+  [ brojMerenihKorisnika ] 
+  (if (= brojMerenihKorisnika 0)
+    (inc brojMerenihKorisnika)
+    brojMerenihKorisnika))
+
+(defn reset
+  [] 
+  (def ukupanBMI 0)
+  (def ukupnaVisina 0)
+  (def ukupnaTezina 0)
+  (def brojMerenihKorisnika 0))
+
+(defn formula
+  [request]
+  (def visina (Double. (:visina (:params request))))
+  (def tezina (Double. (:tezina (:params request))))
+  (def rezultat  
+         (* (/ tezina (*  visina visina ))
+             10000.0))
+  
+   (format "%.6s" (str rezultat)))
+
 (defn registar
   [request]
   (do (if (@korisnici (:kime (:params request)))
@@ -21,47 +79,59 @@
 
 (defn izracunaj
   [request]  
-      (do (def tekst (str "Konverzija broja Double u String " )
-             #_(str (formula (:visina (:params request)) (:tezina (:params request)))))
-		      (response/redirect "/bmi")))
+      (do (def tekstVisina (str  kime  " ,Vasa visina, je za "
+                                (format "%.6s" (razlika (/ ukupnaVisina (provera brojMerenihKorisnika)) visina ))  
+                                 " cm "  razlikaMV " od prosecne!"))  
+          (def tekstTezina (str  kime  " ,Vasa tezina, je za "
+                                 (format "%.6s" (razlika (/ ukupnaTezina (provera brojMerenihKorisnika)) tezina ))
+                                 " kg "  razlikaMV " od prosecne!"))
+          (def tekstBMI (str  kime  " ,Vas BMI, je za "
+                             (format "%.6s"  (razlika (/ ukupanBMI (provera brojMerenihKorisnika)) rezultat )) 
+                              " "  razlikaMV " od prosecnog!"))
+          (def tekst (formula request))
+          (def ukupanBMI (+ ukupanBMI (Double. tekst)))
+          (def brojMerenihKorisnika (+ brojMerenihKorisnika 1))
+          (def ukupnaVisina (+ ukupnaVisina visina))
+          (def ukupnaTezina (+ ukupnaTezina tezina))
+          (response/redirect "/bmi")))  
 
 (en/deftemplate homepage 
   (en/xml-resource "homepage.html")
   [request]
   )
 
-(en/deftemplate bmi 
-  (en/xml-resource "bmi.html")
-  [request]
-  [:#id :h2] (en/content (str "Vas BMI, " kime " je : " tekst) ))
-
 (en/deftemplate kontakt 
   (en/xml-resource "kontakt.html")
   [request]
   )
-
-(def tekst "")
-
-(def text "")
-
-(def kime "" "korisnice")
 
 (en/deftemplate registracija 
   (en/xml-resource "registracija.html")
   [request]
  [:#id :h2] (en/content (str text)))
 
-
-(defn formula
-  [tezina visina]
-  (/ tezina (/ (* visina visina) 10000.0)))
+(en/deftemplate bmi 
+  (en/xml-resource "bmi.html")
+  [request]
+  [:#id :h2] (en/content (str "Vas BMI, " kime " je : " tekst))
+  [:#table :#id2]   (en/content (str prazanString))
+  [:#table :#p2]   (en/content (str "Prosecna visina nasih korisnika je "))
+  [:#table :#p3]   (en/content (str (format "%.6s" (str (/ ukupnaVisina (provera brojMerenihKorisnika) ) ))))
+  [:#table :#p4]   (en/content (str "Prosecna tezina nasih korisnika je "))
+  [:#table :#p5]   (en/content (str (format "%.6s" (str (/ ukupnaTezina (provera brojMerenihKorisnika) ) ))))
+  [:#table :#p6]   (en/content (str "Prosecan BMI nasih korisnika je"))
+  [:#table :#p7]   (en/content (str (format "%.6s" (str (/ ukupanBMI (provera brojMerenihKorisnika)) ))))
+  [:#table :#pk2]  (en/content (str tekstVisina))  
+  [:#table :#pk3]  (en/content (str tekstTezina))
+  [:#table :#pk4]   (en/content (str tekstBMI)))
     
 (defroutes app*
   (GET "/" request (homepage request))
-  (GET "/bmi" request (bmi request))
   (GET "/kontakt" request (kontakt request))
   (GET "/registracija" request (registracija request))
   (GET "/registar" request (registar request))
+  (GET "/bmi" request (bmi request))
   (GET "/izracunaj" request (izracunaj request)))
 
 (def app (compojure.handler/site app*)) 
+
